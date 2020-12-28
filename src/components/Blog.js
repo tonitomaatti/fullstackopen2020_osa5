ea03662a89656ecbe26174fafd2141a5919f9e81
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-28 12:47:27 +0200

    5.7 togglable blog full view

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,30 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [fullView, setFullView] = useState(false)
+
+  const toggleFullView = () => {
+    setFullView(!fullView)
+  }
+
+  const visibility = { display: fullView ? '' : 'none' }
+
+  const label = fullView ? 'hide' : 'view' 
+  
+  return (
+    <div>
+      <div>
+        {blog.title}
+        <button onClick={toggleFullView}>{label}</button>
+      </div>
+      <div style={visibility}>{blog.url}</div>
+      <div style={visibility}>
+        {blog.likes}
+        <button>like</button>
+      </div>
+      <div style={visibility}>{blog.author}</div>
+    </div>
+  )
+}
 
 export default Blog
