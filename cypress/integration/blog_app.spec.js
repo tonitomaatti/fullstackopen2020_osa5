afd78f87951bdb61841960e76d7e9ac9c956a8bb
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-29 01:39:51 +0200

    5.18 login tests

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Cypress Tester',
+      username: 'c_tester',
+      password: 'secretPassword'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -22,5 +28,36 @@ describe('Blog app', function() {
 
     cy.contains('blogs')
       .should('not.exist')
+  })
+
+  describe('Login',function() {
+    beforeEach(function() {
+      localStorage.removeItem('loggedBlogAppUser')
+      cy.reload()
+    })
+
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('c_tester')
+      cy.get('#password').type('secretPassword')
+      cy.get('#login-button').click()
+
+      cy.get('#notification')
+        .should('contain', 'Login succesful')
+        .and('have.css', 'color', 'rgb(0, 128, 0)')
+        .and('have.css', 'border-style', 'solid')
+        .and('not.contain', 'wrong username or password')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('wrong_username')
+      cy.get('#password').type('wrong_secretPassword')
+      cy.get('#login-button').click()
+
+      cy.get('#notification')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+        .and('not.contain', 'Login succesful')
+    })
   })
 })
